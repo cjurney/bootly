3cfdde6f8e4256c6e36a8d2cfe7418efdb41e607
Craig Jurney <cjurney@highwire.com>
2018-08-10 12:53:35 -0700

    Initial commit

---
+++ src/main/java/org/highwire/bootly/BootlyApplication.java
@@ -0,0 +1,12 @@
+package org.highwire.bootly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootlyApplication.class, args);
+    }
+}
---
+++ src/test/java/org/highwire/bootly/BootlyApplicationTests.java
@@ -0,0 +1,16 @@
+package org.highwire.bootly;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootlyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
